faed07c86eef4be45527e26dad827f45ef96aa2d
Eddie Cassar <cassar.eddie.l@macbook-pro.lan>
2025-08-27 19:14:41 +0100

    Node class has forward propogation, a constructor, and the activation functions with their derivatives

--- mlp.py
+++ mlp.py
@@ -20,10 +20,34 @@ class MLP:
         self.final_output = self.softmax(self.final_output)
 
     def backward_propagate(self, X, y, output, learning_rate):
+        '''Method intended to backward propagate using learning rate decay method.
+        Parameters: Double, Double, Double, Double
+        Return: Void'''
         output_error = output - y
         hidden_error = np.dot(output_error, self.weights_hidden_output.T) * self.hidden_output * (1 - self.hidden_output)
 
-        
+        self.weights_output_hidden -= learning_rate*(np.dot(self.hidden_output.T, output_error))
+        self.bias_output -= learning_rate*np.sum(output_error, axis=0, keepdims=True)
+        self.weights_input_hidden -= learning_rate*np.dot(X.T, hidden_error)
+        self.bias_hidden -= learning_rate*np.sum(hidden_error, axis=0, keepdims=True)
+
+    def train_model(self, X, y, epochs, learning_rate):
+        '''Method intended to train MLP model.
+        Parameters: Double, Double, Int, Double
+        Return: Void'''
+        for epoch in range(epochs):
+            output = self.forward_propagate(X)
+            self.backward_propagate(X, y, output, learning_rate)
+            if (epoch+1) % 100 == 0:
+                loss = -np.sum(y * np.log(output))/X.shape[0]
+                print(f'Epoch {epoch+1}, Loss: {loss:.4f}')
+
+    def predict(self, X):
+        '''Method intended to print output.
+        Parameters: Double
+        Return: Double'''
+        output = self.forward(X)
+        return np.argmax(output, axis=1)
 
 
     def sigmoid(self, x):
@@ -38,12 +62,3 @@ class MLP:
         Return: Double'''
         exp_x = np.exp(x-np.max(x))
         return exp_x / exp_x.sum(axis=1, keepdims=True)
-
-    def ReLu(self, x):
-        '''Method intended to take a value x and output a value calculated with ReLu function.
-        Parameters: Double
-        Return: Double'''
-        if x > 0:
-            return x
-        else: 
-            return 0
---
+++ mlp_proper/Node.py
@@ -0,0 +1,58 @@
+import math
+import random
+
+class Node:
+    def __init__(self, num_inputs, activation_function="SIGMOID"):
+        # Weights and biases chosen randomly so that every neuron doesn't remain the same.
+        self.weights = []
+        for i in range(num_inputs):
+            self.weights.append(random.uniform(-1, 1))
+        self.bias = random.uniform(-1, 1)
+
+        # Creates activation function, sets it to upper case to account for capitalisation differences.
+        self.activation_function = activation_function.upper()
+
+        # Input and Output for forward and backward propogation
+        self.input = None
+        self.output = None
+
+        # Gradient used for backward propogation.
+        self.delta = None
+
+    def activation_function(self, x):
+        '''Activation function for whatever is chosed for it.
+        Parameters: Double
+        Return: Double'''
+        if activation_function=="SIGMOID":
+            return 1/(1+math.exp(-x))
+        elif activation_function=="RELU":
+            return max(0, x)
+        elif activation_function=="TANH":
+            return math.tanh(x)
+        else: # Linear
+            return x
+
+    def activation_function_derivative(self, x):
+        '''Derivative of each activation function.
+        Parameters: Double
+        Return: Double'''
+        if activation_function=="SIGMOID":
+            return math.exp(-x)/((1+math.exp(-x))**2)
+        elif activation_function=="RELU":
+            if x > 0:
+                return x 
+            else: 
+                return 0
+        elif activation_function=="TANH":
+            return 1/(1+x**2)
+        else: # Linear
+            return 1
+
+    def forward_propagate(self, inputs):
+        '''Forward propogation, takes input and calculates output.
+        Parameters: List
+        Output: Double'''
+        self.input = inputs
+        z = sum(w*x for w, x in zip(self.weights, inputs)) + self.bias
+        self.output = self.activation_function(z)
+        return self.output
